f53f83b7a04164520ba918bad2aeed5d408366ca
anh010 <anh010@ucsd.edu>
2022-05-03 19:25:04 -0700

    landing page

---
+++ public/script.js
@@ -0,0 +1,10 @@
+function registered(){
+    document.body.style.backgroundImage = "url(public/images/meow2.png)";
+    document.getElementById('msg').innerHTML = "Thank you for registering!";
+
+}
+function welcome(){
+    document.body.style.backgroundImage = "url(public/images/meow2.png)";
+    document.getElementById('msg').innerHTML = "Welcome Back!";
+
+}
