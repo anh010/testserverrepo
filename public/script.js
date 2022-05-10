3f67dfccd09b8c5fe9e1ced908d5d2865f1167e4
anh010 <anh010@ucsd.edu>
2022-05-10 11:26:14 -0700

    fixed mobile view i hope

--- public/script.js
+++ public/script.js
@@ -1,10 +1,29 @@
 function registered(){
-    document.body.style.backgroundImage = "url(public/images/meow2.png)";
-    document.getElementById('msg').innerHTML = "Thank you for registering!";
+    let width = screen.width;
+    if (width < 700 ){
+        console.log(width);
 
+        document.body.style.backgroundImage = "url(public/images/mobile_meow2.png)";
+        document.getElementById('msg').innerHTML = "Thank you for registering!";
+    }
+    else{
+        document.body.style.backgroundImage = "url(public/images/meow2.png)";
+        document.getElementById('msg').innerHTML = "Thank you for registering!";
+
+    }
+    
 }
 function welcome(){
-    document.body.style.backgroundImage = "url(public/images/meow2.png)";
-    document.getElementById('msg').innerHTML = "Welcome Back!";
+    let width = screen.width;
+    if (width < 700 ){
+        console.log(width);
 
+        document.body.style.backgroundImage = "url(public/images/mobile_meow2.png)";
+        document.getElementById('msg').innerHTML = "Welcome Back!";
+    }
+    else{
+        document.body.style.backgroundImage = "url(public/images/mobile_meow2.png)";
+        document.getElementById('msg').innerHTML = "Welcome Back!";
+
+    }
 }
