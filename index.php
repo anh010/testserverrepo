7f10e4b3dd54a91ee3dee65e494cdaff9b038713
anh010 <anh010@ucsd.edu>
2022-05-11 15:17:49 -0700

    test 3

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<!DOCTYPE html>
+<?php
+
+include('connect.php');
+
+$email = $_POST['email'];
+$petID = $_POST['PETID'];
+$sqlquery = "insert into ID_Emails (petid, owner) values ('$pedID','$email');";
+
+if(!mysqli_query($dbcon,$sqlquery)){
+    die('error registering email');
+}
+$newuser = "Registered sucessfully!";
+
+$dbcon->close();
+
+
+?>
+
 <html>
   <head>
     <meta charset="utf-8">
@@ -12,11 +29,11 @@
   <body id ="body">
     <div class="title"> Welcome to Pet Pets!</div>
     <p class="title">To register or login please your email and Pep Pet ID</p>
-    <form method ='post' action = 'updateDB.php' >
+    <form method ='post' action = 'index.php' >
     <div class ="login">
       E-mail: <input type="text" id="email"  name="email"><br>
       Pep Pet ID: <input type="text" id="petid"name="PETID"><br><br>
-
+     
       <Button  type="submit">Register </Button> 
       <Button type="submit"> Login</Button>
     </div>
